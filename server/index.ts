d9ee73074cf4308161c3b924cbf137c5de9d947d
Etiarse <et.arsenault2000@gmail.com>
2025-05-28 22:26:15 -0400

    Add cross-env

--- server/index.ts
+++ server/index.ts
@@ -54,16 +54,17 @@ app.use((req, res, next) => {
     await setupVite(app, server);
   } else {
     serveStatic(app);
+    
   }
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = 5050;
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    reusePort: false,
   }, () => {
     log(`serving on port ${port}`);
   });
